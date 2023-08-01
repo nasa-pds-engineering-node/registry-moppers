c5c3a2646c620030321299a0d3626c4cf68f7335
Al Niessner <Al.Niessner@xxx.xxx>
2023-08-01 15:04:08 -0700

    more whitespace

--- src/pds/registrysweepers/repairkit/__init__.py
+++ src/pds/registrysweepers/repairkit/__init__.py
@@ -39,8 +39,8 @@ To get str_a == str_b, re.compile(str_a).fullmatch
 '''
 
 REPAIR_TOOLS = {
-re.compile('^ops:Data_File_Info/').match: [allarrays.repair],
-re.compile('^ops:Label_File_Info/').match: [allarrays.repair],
+    re.compile('^ops:Data_File_Info/').match: [allarrays.repair],
+    re.compile('^ops:Label_File_Info/').match: [allarrays.repair],
 }
 
 log = logging.getLogger(__name__)
