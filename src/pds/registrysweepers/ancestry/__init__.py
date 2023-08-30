b9d9eb86cca17a8530841d71eec9715ac9eb841e
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-08-29 19:49:58 -0700

    update ancestry sweeper to use new write_updated_docs() implementation

--- src/pds/registrysweepers/ancestry/__init__.py
+++ src/pds/registrysweepers/ancestry/__init__.py
@@ -1,11 +1,11 @@
 import logging
 from itertools import chain
-from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import Iterable
 from typing import List
 from typing import Optional
+from typing import Set
 from typing import Tuple
 from typing import Union
 
@@ -16,7 +16,8 @@ from pds.registrysweepers.ancestry.generation import get_nonaggregate_ancestry_r
 from pds.registrysweepers.utils import configure_logging
 from pds.registrysweepers.utils import Host
 from pds.registrysweepers.utils import parse_args
-from pds.registrysweepers.utils import write_updated_docs_legacy
+from pds.registrysweepers.utils import Update
+from pds.registrysweepers.utils import write_updated_docs
 
 log = logging.getLogger(__name__)
 
@@ -46,9 +47,26 @@ def run(
     nonaggregate_records = get_nonaggregate_ancestry_records(host, collection_records, registry_mock_query_f)
 
     ancestry_records = chain(bundle_records, collection_records, nonaggregate_records)
+    updates = generate_updates(ancestry_records, ancestry_records_accumulator, bulk_updates_sink)
+
+    if bulk_updates_sink is None:
+        log.info("Writing bulk updates to database...")
+        write_updated_docs(host, updates)
+    else:
+        # consume generator to dump bulk updates to sink
+        for _ in updates:
+            pass
+
+    log.info("Ancestry sweeper processing complete!")
+
+
+def generate_updates(
+    ancestry_records: Iterable[AncestryRecord], ancestry_records_accumulator=None, bulk_updates_sink=None
+) -> Iterable[Update]:
+    updates: Set[str] = set()
 
     log.info("Generating document bulk updates for AncestryRecords...")
-    updates: Dict[str, Dict[str, Any]] = {}
+
     for record in ancestry_records:
         # Tee the stream of records into the accumulator, if one was provided (functional testing).
         if ancestry_records_accumulator is not None:
@@ -58,29 +76,23 @@ def run(
             log.warning(f"Collection {record.lidvid} is not referenced by any bundle.")
 
         doc_id = str(record.lidvid)
-        update = {
+        update_content = {
             METADATA_PARENT_BUNDLE_KEY: [str(id) for id in record.parent_bundle_lidvids],
             METADATA_PARENT_COLLECTION_KEY: [str(id) for id in record.parent_collection_lidvids],
         }
 
         # Tee the stream of bulk update KVs into the accumulator, if one was provided (functional testing).
         if bulk_updates_sink is not None:
-            bulk_updates_sink.append((doc_id, update))
+            bulk_updates_sink.append((doc_id, update_content))
 
         if doc_id in updates:
-            existing_update = updates[doc_id]
             log.error(
-                f"Multiple updates detected for doc_id {doc_id} - cannot create update! (got {update}, {existing_update} already exists)"
+                f"Multiple updates detected for doc_id {doc_id} - cannot create update! (new content {update_content} will not be written)"
             )
             continue
 
-        updates[doc_id] = update
-
-    if updates and bulk_updates_sink is None:
-        log.info("Writing bulk updates to database...")
-        write_updated_docs_legacy(host, updates)
-
-    log.info("Ancestry sweeper processing complete!")
+        updates.add(doc_id)
+        yield Update(id=doc_id, content=update_content)
 
 
 if __name__ == "__main__":
