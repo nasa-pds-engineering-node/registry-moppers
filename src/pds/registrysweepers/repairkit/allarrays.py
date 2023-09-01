063c9709ed0a7a05d99e20eee761482316309b41
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-08-31 20:43:20 -0700

    demote log message

--- src/pds/registrysweepers/repairkit/allarrays.py
+++ src/pds/registrysweepers/repairkit/allarrays.py
@@ -8,6 +8,6 @@ log = logging.getLogger(__name__)
 def repair(document: Dict, fieldname: str) -> Dict:
     log.debug(f"checking {fieldname}")
     if isinstance(document[fieldname], str):
-        log.info(f"found string for {fieldname} where it should be an array")
+        log.debug(f"found string for {fieldname} where it should be an array")
         return {fieldname: [document[fieldname]]}
     return {}
