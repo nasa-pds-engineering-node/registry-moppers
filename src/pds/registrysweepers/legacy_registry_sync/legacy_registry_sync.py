a1a987857da9f9a38662ec8c3c444fc22b59fa3a
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2023-10-09 17:33:34 -0400

    migrate elasticsearch client to opensearch-py for legacy synx sweeper. Use pypi temporary release of a dependency.

--- src/pds/registrysweepers/legacy_registry_sync/legacy_registry_sync.py
+++ src/pds/registrysweepers/legacy_registry_sync/legacy_registry_sync.py
@@ -1,8 +1,8 @@
 import logging
 from typing import Union
 
-import elasticsearch.helpers  # type: ignore
-from elasticsearch import Elasticsearch
+import opensearchpy.helpers
+from opensearchpy import OpenSearch
 from pds.registrysweepers.legacy_registry_sync.opensearch_loaded_product import get_already_loaded_lidvids
 from pds.registrysweepers.legacy_registry_sync.solr_doc_export_to_opensearch import SolrOsWrapperIter
 from pds.registrysweepers.utils import configure_logging
@@ -28,20 +28,14 @@ def create_legacy_registry_index(es_conn=None):
 
 
 def run(
-    base_url: str,
-    username: str,
-    password: str,
-    verify_host_certs: bool = True,
+    client: OpenSearch,
     log_filepath: Union[str, None] = None,
     log_level: int = logging.INFO,
 ):
     """
     Runs the Solr Legacy Registry synchronization with OpenSearch.
 
-    @param base_url: of the OpenSearch service
-    @param username:
-    @param password:
-    @param verify_host_certs:
+    @param client: OpenSearch client from the opensearchpy library
     @param log_filepath:
     @param log_level:
     @return:
@@ -49,19 +43,17 @@ def run(
 
     configure_logging(filepath=log_filepath, log_level=log_level)
 
-    es_conn = Elasticsearch(hosts=base_url, verify_certs=verify_host_certs, http_auth=(username, password))
-
     solr_itr = SlowSolrDocs(SOLR_URL, "*", rows=100)
 
-    create_legacy_registry_index(es_conn=es_conn)
+    create_legacy_registry_index(es_conn=client)
 
     prod_ids = get_already_loaded_lidvids(
-        product_classes=["Product_Context", "Product_Collection", "Product_Bundle"], es_conn=es_conn
+        product_classes=["Product_Context", "Product_Collection", "Product_Bundle"], es_conn=client
     )
 
     es_actions = SolrOsWrapperIter(solr_itr, OS_INDEX, found_ids=prod_ids)
-    for ok, item in elasticsearch.helpers.streaming_bulk(
-        es_conn, es_actions, chunk_size=50, max_chunk_bytes=50000000, max_retries=5, initial_backoff=10
+    for ok, item in opensearchpy.helpers.streaming_bulk(
+        client, es_actions, chunk_size=50, max_chunk_bytes=50000000, max_retries=5, initial_backoff=10
     ):
         if not ok:
             log.error(item)
