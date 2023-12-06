c334db4ef21f1fcf8f2e3f5c9749504270cb501c
Alex Dunn <75815303+alexdunnjpl@users.noreply.github.com>
2023-12-06 14:05:31 -0800

    add ops:Tracking_Meta/ops:archive_status to EXCLUDED_PROPERTIES

--- src/pds/registrysweepers/repairkit/allarrays.py
+++ src/pds/registrysweepers/repairkit/allarrays.py
@@ -6,7 +6,7 @@ from typing import Dict
 log = logging.getLogger(__name__)
 
 # exclude the following properties from array conversion even if targeted - they are expected to be string-typed
-EXCLUDED_PROPERTIES = {"lid", "vid", "lidvid", "title", "product_class", "_package_id"}
+EXCLUDED_PROPERTIES = {"lid", "vid", "lidvid", "title", "product_class", "_package_id", "ops:Tracking_Meta/ops:archive_status"}
 
 
 def repair(document: Dict, fieldname: str) -> Dict:
