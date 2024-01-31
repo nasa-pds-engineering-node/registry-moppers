b90fc9f646b56167df2fcc5a48d1a75237446541
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2024-01-30 18:07:23 -0800

    minimally-update provenance tests to interface with provenance reimplementation

--- tests/pds/registrysweepers/provenance/test_provenance.py
+++ tests/pds/registrysweepers/provenance/test_provenance.py
@@ -1,8 +1,11 @@
+import itertools
 import json
 import os.path
 import unittest
 
 from pds.registrysweepers import provenance
+from pds.registrysweepers.provenance import ProvenanceRecord
+from pds.registrysweepers.provenance import SWEEPERS_PROVENANCE_VERSION
 from pds.registrysweepers.utils.db import Update
 
 
@@ -24,7 +27,6 @@ class ProvenanceBasicFunctionalTestCase(unittest.TestCase):
     ]
 
     def test_correct_provenance_produced(self):
-        successors_by_lidvid = provenance.get_successors_by_lidvid(self.extant_lidvids)
         expected_provenance = {
             "urn:nasa:pds:bundle::1.0": "urn:nasa:pds:bundle::1.1",
             "urn:nasa:pds:bundle::1.1": "urn:nasa:pds:bundle::2.0",
@@ -33,19 +35,33 @@ class ProvenanceBasicFunctionalTestCase(unittest.TestCase):
             "urn:nasa:pds:bundle:collection:product::100.0": "urn:nasa:pds:bundle:collection:product::100.1",
             "urn:nasa:pds:bundle:collection:product::100.1": "urn:nasa:pds:bundle:collection:product::200.0",
         }
-        self.assertDictEqual(expected_provenance, successors_by_lidvid)
 
         def crude_update_hash(update: Update) -> str:
             d = {"id": update.id, "content": update.content}
             return json.dumps(d, sort_keys=True)
 
-        updates = provenance.generate_updates(successors_by_lidvid)
+        records = [ProvenanceRecord.from_source({"lidvid": lidvid}) for lidvid in self.extant_lidvids]
+        record_chains = provenance.create_record_chains(records)
+        for record_chain in record_chains:
+            provenance.link_records_in_chain(record_chain)
+
+        updates = provenance.generate_updates(itertools.chain(*record_chains))
+        # previously, updates were not generated for latest products, so we need to exclude those from comparison for
+        # consistency
+        legacy_updates = [u for u in updates if u.content["ops:Provenance/ops:superseded_by"] is not None]
         expected_updates = [
-            Update(id=k, content={"ops:Provenance/ops:superseded_by": v}) for k, v in expected_provenance.items()
+            Update(
+                id=k,
+                content={
+                    "ops:Provenance/ops:superseded_by": v,
+                    "ops:Provenance/ops:registry_sweepers_provenance_version": SWEEPERS_PROVENANCE_VERSION,
+                },
+            )
+            for k, v in expected_provenance.items()
         ]
 
         self.assertSetEqual(
-            set(crude_update_hash(u) for u in expected_updates), set(crude_update_hash(u) for u in updates)
+            set(crude_update_hash(u) for u in expected_updates), set(crude_update_hash(u) for u in legacy_updates)
         )
 
 
