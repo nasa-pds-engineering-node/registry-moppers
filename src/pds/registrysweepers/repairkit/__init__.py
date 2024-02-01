e41acab1117478f604ffd0404033e8099b838028
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2024-02-01 12:13:48 -0800

    change repairkit to use search-after pagination this is necessary as scrolling is not recommended for deep pagination (>10000 hits, per docs)

--- src/pds/registrysweepers/repairkit/__init__.py
+++ src/pds/registrysweepers/repairkit/__init__.py
@@ -13,18 +13,16 @@ from typing import Iterable
 from typing import Union
 
 from opensearchpy import OpenSearch
+from pds.registrysweepers.repairkit import allarrays
 from pds.registrysweepers.repairkit.versioning import SWEEPERS_REPAIRKIT_VERSION
 from pds.registrysweepers.repairkit.versioning import SWEEPERS_REPAIRKIT_VERSION_METADATA_KEY
 from pds.registrysweepers.utils import configure_logging
 from pds.registrysweepers.utils import parse_args
-from pds.registrysweepers.utils import query_registry_db
+from pds.registrysweepers.utils.db import query_registry_db_with_search_after
+from pds.registrysweepers.utils.db import write_updated_docs
 from pds.registrysweepers.utils.db.client import get_opensearch_client
 from pds.registrysweepers.utils.db.indexing import ensure_index_mapping
 from pds.registrysweepers.utils.db.update import Update
-from pds.registrysweepers.utils.misc import get_sweeper_version_metadata_key
-
-from . import allarrays
-from ..utils.db import write_updated_docs
 
 """
 dictionary repair tools is {field_name:[funcs]} where field_name can be:
@@ -97,7 +95,7 @@ def run(
 
     # page_size and bulk_chunk_max_update_count constraints are necessary to avoid choking nodes with very-large docs
     # i.e. ATM and GEO
-    all_docs = query_registry_db(client, "registry", unprocessed_docs_query, {}, page_size=1000)
+    all_docs = query_registry_db_with_search_after(client, "registry", unprocessed_docs_query, {}, page_size=1000)
     updates = generate_updates(all_docs, SWEEPERS_REPAIRKIT_VERSION_METADATA_KEY, SWEEPERS_REPAIRKIT_VERSION)
     ensure_index_mapping(client, "registry", SWEEPERS_REPAIRKIT_VERSION_METADATA_KEY, "integer")
     write_updated_docs(client, updates, index_name="registry", bulk_chunk_max_update_count=20000)
